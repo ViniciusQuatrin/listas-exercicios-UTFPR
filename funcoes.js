8e8e5d4ad8843ff693c1ed9b0fdb29568c7f5e88
Vinícius Quatrin <viniquatrin48@gmail.com>
2022-09-24 02:29:07 -0300

    conclusão do código

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,21 @@
 function somar() {
-    soma += num
+    soma += num;
+}
+function calcularMedia() {
+    media = soma / (i-1);
+}
+function acharMaiorMenor() {
+    if (num > maior){
+        maior = num;
+    }
+    if (num < menor){
+        menor = num;
+    }
+}
+function contarPar(){
+    if (num % 2 == 0){
+        qtdPar++;
+    } else {
+        qtdImpar++;
+    }
 }
